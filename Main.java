4707ee9e5ccb3972c4bcebf5753445add825619e
jkebx22 <42923140+jkebx22@users.noreply.github.com>
2019-01-30 18:32:41 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int iVal = 150;
+
+	do {
+	    System.out.print(iVal);
+	    System.out.print(" * 2 = ");
+	    iVal *= 2;
+	    System.out.println(iVal);
+    } while (iVal < 100);
+    }
+}
